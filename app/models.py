145fe11eb2781207628c180e02e41222f7244fed
Jeremiah Brian <brianjeremy123789@gmail.com>
2020-10-01 19:57:00 +0300

    user

--- app/models.py
+++ app/models.py
@@ -19,8 +19,7 @@ class User(UserMixin,db.Model):
     is_staff = db.Column(db.Boolean, default=False)
     is_student = db.Column(db.Boolean, default=False)
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
-    department_id = db.Column(db.Integer, db.ForeignKey('departments.id'))    
-    wishlist_id = db.Column(db.Integer, db.ForeignKey('wishlists.id'))    
+    department_id = db.Column(db.Integer, db.ForeignKey('departments.id'))      
     
     @property
     def password(self):
@@ -105,12 +104,12 @@ class Comment(db.Model):
         return f'Comment{self.comments}'
     
 class Wishlist(db.Model):
-    __tablename__ = 'wishlist' 
+    __tablename__ = 'wishlists' 
     
     id = db.Column(db.Integer, primary_key=True)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
     
     def __repr__(self):
         return f"Post('{self.content}', '{self.date_posted}')"
