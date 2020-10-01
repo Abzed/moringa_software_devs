1f6ba9c4a8ab4ed08099890a7a9648b53ea65977
vsidera <siderravictor@gmail.com>
2020-10-01 15:55:00 +0300

    remove class wishlist

--- app/models.py
+++ app/models.py
@@ -103,16 +103,16 @@ class Comment(db.Model):
     def __repr__(self):
         return f'Comment{self.comments}'
     
-class Wishlist(db.Model):
-     __tablename__ = 'wishlist'
+# class Wishlist(db.Model):
+#      __tablename__ = 'wishlist'
 
-    id = db.Column(db.Integer, primary_key=True)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+#     id = db.Column(db.Integer, primary_key=True)
+#     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+#     content = db.Column(db.Text, nullable=False)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    def __repr__(self):
-        return f"Post('{self.content}', '{self.date_posted}')"
+#     def __repr__(self):
+#         return f"Post('{self.content}', '{self.date_posted}')"
     
 #class Comment-id,commnt.... user_id.... post_id
 
