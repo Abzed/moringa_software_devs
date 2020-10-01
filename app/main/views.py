04245ebb11554d793f921ff6df92eb2f1d215215
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-10-02 02:35:42 +0300

    Update views.py
    Added update profile and update profile pic

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,request,redirect,url_for,abort,flash
 from . import main
-from .forms import BioForm, CommentForm
+from .forms import BioForm, CommentForm, UpdateProfile
 from ..models import User,Role,Department,Post,Comment
 from flask_login import login_required,current_user
 from .. import db,photos
@@ -16,3 +16,44 @@ def index():
 @login_required
 def categories():
     return render_template('category-page.html')
+
+@main.route('/staff/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("staff/profile.html", user = user)
+
+@main.route('/staff/<uname>/update', methods=['GET', 'POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username=uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile', uname=user.username))
+
+    return render_template('staff/edit_bio.html', form=form)
+
+@main.route('/staff/<uname>/update',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+
+        db.session.commit()
+
+    return redirect(url_for('.profile',uname=uname))
