f87c1250aa282739586b4ea4aae5044dd430c60d
Abzed <ibraabzed@gmail.com>
2020-10-01 16:06:05 +0300

    add wishlist

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,6 +1,9 @@
-from flask import abort, render_template
+from flask import abort, render_template,flash,url_for,abort,request,redirect
 from flask_login import current_user, login_required
 from . import admin
+from ..models import User,Role,Department,Post,Comment
+from .. import db,photos
+from .forms import DeleteForm
 
 # add admin dashboard view
 @admin.route('/admin')
@@ -8,4 +11,30 @@ from . import admin
 def admin_dashboard():
     if not current_user.is_admin:
         abort(403)
-    return render_template('form.html', title="Admin Dashboard")
+    return render_template('form.html', title="Admin Dashboard",form=DeleteForm)
+
+@admin.route('/users/delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def delete_user(email):
+    """
+    Deactivate a user from the database
+    """
+    user = User.query.all()
+    user_mail = User.query.get(email)
+    
+    if not current_user.is_admin:
+        abort(403)
+        
+    form = DeleteForm()
+    if form.validate_on_submit:
+        db.session.delete(user_mail)
+        db.session.commit()
+    
+    flash('You have successfully deactivated the user.')
+    
+    # redirect to the home page    
+    return redirect(url_for('admin.admin_dashboard',user=user,user_mail=user_mail,form=form))
+        
+        
+    
+    
