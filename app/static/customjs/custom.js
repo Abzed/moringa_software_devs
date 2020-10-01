7bee036e4e20ceefca39252144ca5cc0063fb1d0
Jeremiah Brian <brianjeremy123789@gmail.com>
2020-10-01 23:07:09 +0300

    landing page

--- app/static/customjs/custom.js
+++ app/static/customjs/custom.js
@@ -9,7 +9,7 @@ function scrollFunction() {
     document.getElementById("navTop").style.zIndex  = "99";
     document.getElementById("navTop").style.transition ="background 0.4s"
   } else {
-    document.getElementById("navTop").style.background = "transparent";
+    document.getElementById("navTop").style.background = "linear-gradient(90deg, rgba(213,213,227,0.8883928571428571) 70%, rgba(231,240,234,0.804359243697479) 96%)";
     document.getElementById("navTop").style.boxShadow  = "0 8px 0px 0px rgba(0, 0, 0, 0.664)";
     document.getElementById("navTop").style.webkitBoxShadow  = "0 0px 0px 0px rgba(0, 0, 0, 0.603)";
     document.getElementById("navTop").style.mozBoxShadow  = "0 0px 0px 0px rgba(0, 0, 0, 0.678)";
