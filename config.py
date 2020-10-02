5a6c2429e168f048a48b795890e001583509dd7d
Jeremiah Brian <brianjeremy123789@gmail.com>
2020-10-02 09:42:10 +0300

    landing page

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ import os
 class Config:
     
     API_BASE_URL = ''
-    SECRET_KEY = 'ninjaits'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'jeremy2bjunior@gmail.com'
-    MAIL_PASSWORD = 'Juniors2b12'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -21,7 +21,7 @@ class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI_TEST")
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://oem:Jeremih23@localhost/msdevs'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
     DEBUG = True
     
 config_options = {
